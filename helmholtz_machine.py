66f70c80f2a0958368e7f9fbed41f8f19d63819a
Saarang Panchavati <saarangp@gmail.com>
2020-12-04 20:27:53 -0600

    made continuous so grayscale works!

--- helmholtz_machine.py
+++ helmholtz_machine.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+import math
 
 class helmholtz(object):
     def __init__(self, epsilon, size):
@@ -11,40 +12,63 @@ class helmholtz(object):
         self.dreams = []
         self.epsilon = epsilon
 
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def sample(self, p, type = 'binomial'):
+        #Takes in probability p and outputs a sample from a distribution over
+        type = 'beta'
+        try:
+            if p == 0:
+                p = 1e-6
+                print("Had to make smol number")
+        except:
+            p[p==0] = 1e-6
+
+        # p = p.numpy()
+        if type == 'binomial':
+            return np.random.binomial(1,p)
+        if type == 'beta':
+            return np.random.beta(p, 1)
+
     def wake_phase(self, X):
-        # Recognition Pass
-        p = (tf.math.sigmoid(np.dot(self.V_R, X)))
-        X_layer1 = p.numpy() #np.random.binomial(1, p)
-        p = (tf.math.sigmoid(np.dot(self.W_R, X_layer1)))
-        X_layer2 = p.numpy() #np.random.binomial(1, p)
+        try:
+            # Recognition Pass
+            p = self.sigmoid(np.dot(self.V_R, X))
+            X_layer1 = self.sample(p)
+            p = self.sigmoid(np.dot(self.W_R, X_layer1))
+            X_layer2 = self.sample(p)
 
-        # Generative Pass
-        zeta = (tf.math.sigmoid(self.B_G))
-        psi = (tf.math.sigmoid(np.dot(self.W_G, X_layer2)))
-        delta = (tf.math.sigmoid(np.dot(self.V_G, X_layer1)))
+            # Generative Pass
+            zeta = (self.sigmoid(self.B_G))
+            psi = (self.sigmoid(np.dot(self.W_G, X_layer2)))
+            delta = (self.sigmoid(np.dot(self.V_G, X_layer1)))
 
-        self.B_G += self.epsilon * (X_layer2 - zeta)
-        self.W_G += self.epsilon * np.dot(X_layer1 - psi, X_layer2)
-        self.V_G += self.epsilon * np.dot(X - delta, X_layer1)
+            self.B_G += self.epsilon * (X_layer2 - zeta)
+            self.W_G += self.epsilon * np.dot(X_layer1 - psi, X_layer2)
+            self.V_G += self.epsilon * np.dot(X - delta, X_layer1)
+        except:
+            print(p)
+            raise
 
 
     def sleep_phase(self):
-        p = (tf.math.sigmoid(self.B_G))
+        p = (self.sigmoid(self.B_G))
 
         #DREAM!
-        X = np.random.binomial(1, p)
+        X = self.sample(p)
         # self.dreams.append(X)
 
-        p = (tf.math.sigmoid(np.dot(self.W_G, X)))
-        X_layer1 = p.numpy() #np.random.binomial(1, p)
+        p = (self.sigmoid(np.dot(self.W_G, X)))
+        X_layer1 = self.sample(p)
 
-        p = (tf.math.sigmoid(np.dot(self.V_G, X_layer1)))
-        X_layer2 = p.numpy() #np.random.binomial(1, p)
+        p = (self.sigmoid(np.dot(self.V_G, X_layer1)))
+        X_layer2 = self.sample(p)
 
         self.dreams.append(X_layer2)
 
-        psi = (tf.math.sigmoid(np.dot(self.V_R, X_layer2)))
-        zeta = (tf.math.sigmoid(np.dot(self.W_R, X_layer1)))
+        psi = (self.sigmoid(np.dot(self.V_R, X_layer2)))
+        zeta = (self.sigmoid(np.dot(self.W_R, X_layer1)))
 
         self.V_R += self.epsilon * np.dot(X_layer1 - psi, X_layer2)
         self.W_R += self.epsilon * np.dot(X - zeta, X_layer1)
--- mnist_dream_generation.py
+++ mnist_dream_generation.py
@@ -9,15 +9,15 @@ import matplotlib.animation as animation
 # Load in MNIST dataset
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 # X = (np.concatenate((x_train, x_test), axis = 0).reshape((len(x_train)+len(x_test), 28*28)) > .5).astype(float)
-X = ((np.concatenate((x_train, x_test), axis = 0)) > .5).astype(float) 
+X = ((np.concatenate((x_train, x_test), axis = 0))).astype(float) 
 y = np.concatenate((y_train, y_test), axis = 0)
 print(X.shape, y.shape)
 
 # Isolate Ones
 digit = X[np.where(y == 1)]
-digit = (digit.reshape((len(digit), 28*28)) > .5).astype(float)
+digit = (digit.reshape((len(digit), 28*28))).astype(float)
 print(digit.shape)
-plt.imshow(digit.reshape((len(digit),28,28))[0])
+plt.imshow(digit.reshape((len(digit),28,28))[0]/255, cmap='gray')
 plt.show()
 
 h = hm.helmholtz(.1, 784)
@@ -28,16 +28,16 @@ dreams = h.dreams
 print(len(dreams)), len(dreams[0])
 
 plt.figure()
-plt.imshow(dreams[-1].reshape(28,28))
+plt.imshow(dreams[-1].reshape(28,28), cmap='gray')
 plt.show()
 
 frames = [] # for storing the generated images
 fig = plt.figure()
-for dream in tqdm(dreams[::100]):
+for dream in tqdm(dreams[:1000:10]):
     dream = dream.reshape(28,28)
-    frames.append([plt.imshow(dream,animated=True)])
+    frames.append([plt.imshow(dream,animated=True, cmap='gray')])
 
-ani = animation.ArtistAnimation(fig, frames, interval=10, blit=True)
+ani = animation.ArtistAnimation(fig, frames, interval=50, blit=True)
 ani.save('dreaming.gif')
 print('Animation Creation Finished')
 
