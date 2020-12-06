2fa5edd5edbe7ef743a7e1d5ee64467aba655b15
hmallen99 <henmanallen@berkeley.edu>
2020-12-05 18:35:16 -0800

    Class structure for sparse coding model

--- sparse_coding.py
+++ sparse_coding.py
@@ -1,4 +1,5 @@
 import numpy as np
+from progressbar import progressbar
 
 def calc_g(u, lmbda):
     return np.maximum(u - lmbda, 0)
@@ -29,11 +30,25 @@ def calc_Phi(X, a_dim, alpha=0.001, num_steps=2000):
     """
     Phi = np.ones((X.shape[1], a_dim))
     Phi = Phi @ np.diag(1 / np.linalg.norm(Phi, ord=2, axis=0))
-    for i in range(num_steps):
-        if i % 100 == 0:
-            print(i)
+    for i in progressbar(range(num_steps)):
         a = calc_LCA(Phi, X)
         Phi = Phi + alpha * (X.T - Phi @ a) @ a.T
         Phi = Phi @ np.diag(1 / np.linalg.norm(Phi, ord=2, axis=0))
 
     return Phi
+
+
+class SparseCodingModel(object):
+    def __init__(self, input_size, n_activations, alpha):
+        self.input_size = input_size
+        self.n_activations = n_activations
+        self.alpha = alpha
+
+    def train(self, X):
+        self.Phi = calc_Phi(X, self.n_activations)
+    
+    def predict(self, X, num_steps=1000):
+        return calc_LCA(self.Phi, X, num_steps=num_steps)
+
+    def generate(self, A):
+        return np.dot(self.Phi, A)
