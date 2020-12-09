c0a717e24f65082b0893f753cdd343c4e081b8c6
Ashwath Radhachandran <ashwath.radha@berkeley.edu>
2020-12-08 16:30:27 -0800

    converting to tensors to use gpu

--- helmholtz.py
+++ helmholtz.py
@@ -81,7 +81,7 @@ class helmholtz(object):
         # print(f"X.shape: {X.shape}")
         for layer in self.layers:
             # print(type(outputs[-1]), type(layer.R))
-            sig = self.sigmoid(torch.dotmatmul(outputs[-1], layer.R))
+            sig = self.sigmoid(torch.matmul(outputs[-1], layer.R))
             # print("recognition: ", sig.shape, outputs[-1].shape, layer.R.shape)
             outputs.append(torch.from_numpy(self.sample(sig)).to(try_gpu()))
 
@@ -91,7 +91,7 @@ class helmholtz(object):
         
         for i, layer in enumerate(self.layers):
             # print("Generative a:", outputs[i+1].shape, layer.G.shape, outputs[i].shape)
-            delta = self.sigmoid(torch.dotmatmul(outputs[i+1], layer.G.T))
+            delta = self.sigmoid(torch.matmul(outputs[i+1], layer.G.T))
             # print("Generative b:", outputs[i + 1].shape, layer.G.shape, outputs[i].shape, delta.shape)
             layerG_upd = self.epsilon * np.dot(outputs[i + 1].T, outputs[i] - delta)
             # print(f"layerG Updated: {layerG_upd.shape}")
@@ -103,7 +103,7 @@ class helmholtz(object):
         #DREAM!
         outputs = [torch.from_numpy(self.sample(p)).to(try_gpu())]
         for layer in self.layers[::-1]:
-            p = (self.sigmoid(torch.dotmatmul(layer.G, outputs[-1])))
+            p = (self.sigmoid(torch.matmul(layer.G, outputs[-1])))
             
             outputs.append(torch.from_numpy(self.sample(p)).to(try_gpu()))
         
@@ -111,11 +111,11 @@ class helmholtz(object):
         #W_R recent output
         for i,layer in enumerate(self.layers[::-1]):
             #print("sleep: ", layer.R.shape, outputs[i+1].shape)
-            psi = self.sigmoid(torch.dotmatmul(outputs[i + 1].T, layer.R))
+            psi = self.sigmoid(torch.matmul(outputs[i + 1].T, layer.R))
             
             #print("psi: ", psi.shape, "outputs[i]: ", outputs[i].shape,  "outputs[i+1]: ", outputs[i+1].shape)
             
-            layerR_upd = self.epsilon * torch.dotmatmul(outputs[i+1], outputs[i].T - psi)
+            layerR_upd = self.epsilon * torch.matmul(outputs[i+1], outputs[i].T - psi)
             #print(f"layerR Updated: {layerR_upd.shape}")
             layer.R += layerR_upd
             
