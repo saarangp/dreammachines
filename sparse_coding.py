475b4b35df2158593a4d4433032fac25e68cd5b1
hmallen99 <henmanallen@berkeley.edu>
2020-12-10 01:59:20 -0800

    More improvements to sparse coding model

--- sparse_coding.py
+++ sparse_coding.py
@@ -1,6 +1,10 @@
 import numpy as np
 from progressbar import progressbar
 
+def get_selections(X, n):
+    selections = np.random.choice(np.arange(X.shape[0]), n, replace=False)
+    return X[selections]
+
 def calc_g(u, lmbda):
     return np.sign(u) * np.maximum(np.abs(u) - lmbda, 0)
 
@@ -15,8 +19,8 @@ def calc_LCA(Phi, X, lmbda=0.1, alpha=0.001, num_steps=1000):
     G = Phi.T @ Phi
     G = G - np.eye(G.shape[0])
     
-    a = np.random.rand(Phi.shape[1], X.shape[0])
-    u = np.random.rand(a.shape[0], a.shape[1])
+    a = np.zeros((Phi.shape[1], X.shape[0]))
+    u = np.zeros((a.shape[0], a.shape[1]))
     prev_u = np.zeros(u.shape)
     for i in range(num_steps):
         u = (1 - alpha) * u + alpha * update_func(Phi, X, G, a)
@@ -26,29 +30,31 @@ def calc_LCA(Phi, X, lmbda=0.1, alpha=0.001, num_steps=1000):
         prev_u = u
     return a
 
-def calc_Phi(X, a_dim, alpha=0.001, num_steps=2000):
+def calc_Phi(X, a_dim, alpha=0.001, num_steps=2000, batch_size=100):
     """
     Calculate the feature dictionary for an image set X
     using nested gradient descent.
     """
     Phi = np.random.rand(X.shape[1], a_dim)
+    Phi = Phi - np.mean(Phi)
     Phi = Phi @ np.diag(1 / np.linalg.norm(Phi, ord=2, axis=0))
     for i in progressbar(range(num_steps)):
-        a = calc_LCA(Phi, X)
-        Phi = Phi + alpha * (X.T - Phi @ a) @ a.T
+        next_X = get_selections(X, batch_size)
+        a = calc_LCA(Phi, next_X)
+        Phi = Phi + alpha * (next_X.T - Phi @ a) @ a.T
         Phi = Phi @ np.diag(1 / np.linalg.norm(Phi, ord=2, axis=0))
 
     return Phi
 
 
 class SparseCodingModel(object):
-    def __init__(self, input_size, n_activations, alpha):
-        self.input_size = input_size
+    def __init__(self, batch_size, n_activations, alpha):
+        self.batch_size = batch_size
         self.n_activations = n_activations
         self.alpha = alpha
 
     def train(self, X, alpha=0.001, num_steps=2000):
-        self.Phi = calc_Phi(X, self.n_activations, alpha, num_steps)
+        self.Phi = calc_Phi(X, self.n_activations, alpha, num_steps, self.batch_size)
     
     def predict(self, X, lmbda=0.1, alpha=0.001, num_steps=1000):
         return calc_LCA(self.Phi, X, lmbda, alpha, num_steps)
